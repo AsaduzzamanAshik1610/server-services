e8d3770b1a0c8c8eb33583a116596b47063d8ce7
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-11-09 11:56:21 +0600

    added the gitignore

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+// user:servicedbuser
+// password:j5BoEDTqCDzYC0GJ
+
+const uri = "mongodb+srv://servicedbuser:j5BoEDTqCDzYC0GJ@cluster0.ignp2kb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+   try{
+     const userCollection = client.db('nodeMongoCrud').collection('users');
+     const user = {
+        name: 'testing test',
+        email: 'testing@gmail.com'
+     }
+     const result = await userCollection.insertOne(user)
+     console.log(result);
+   }
+   finally{
+
+   }
+}
+run().catch(err => console.log(err));
+
+app.get('/', (req, res)=>{
+    res.send('Hellow review server side')
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening to port ${port}`);
+})
