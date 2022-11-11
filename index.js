2b885b736ca38a141defcebfd12487c1e5d64f7a
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-11-11 20:39:28 +0600

    added the review part

--- index.js
+++ index.js
@@ -13,42 +13,47 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWARD}@cluster0.ignp2kb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-   try{
-     const userCollection = client.db('tourservice').collection('users');
-      app.get('/services/upcoming', async(req, res)=>{
-       const query = {}
-       const cursor = userCollection.find(query);
-       const services= await cursor.limit(3).toArray();
-       res.send(services);
-     })
-     
-      app.get('/services', async(req, res)=>{
-       const query = {}
-       const cursor = userCollection.find(query);
-       const services= await cursor.toArray();
-       res.send(services);
-     })
+async function run() {
+  try {
+    const userCollection = client.db('tourservice').collection('users');
+    const reviewCollection = client.db('tourservice').collection('review')
+    app.get('/services/upcoming', async (req, res) => {
+      const query = {}
+      const cursor = userCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    })
 
-     app.get('/services/:id', async(req, res)=>{
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = userCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+    app.get('/services/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const service = await userCollection.findOne(query);
       res.send(service);
-   })
+    })
+    app.post('/review', async(req, res)=>{
+      const order = req.body;
+      const result = await reviewCollection.insertOne(order)
+      res.send(result);
+    })
 
-     
-   }
-   finally{
+  }
+  finally {
 
-   }
+  }
 }
 run().catch(err => console.log(err));
 
-app.get('/', (req, res)=>{
-    res.send('Hellow review server side')
+app.get('/', (req, res) => {
+  res.send('Hellow review server side')
 });
 
-app.listen(port, ()=>{
-    console.log(`Listening to port ${port}`);
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 })
