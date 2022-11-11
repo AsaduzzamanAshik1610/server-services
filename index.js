5c39848eb0815ade7cd1711583212601ceff674f
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-11-11 17:20:41 +0600

    added the seall services

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,13 +15,29 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
    try{
-     const userCollection = client.db('nodeMongoCrud').collection('users');
-     const user = {
-        name: 'testing test',
-        email: 'testing@gmail.com'
-     }
-     const result = await userCollection.insertOne(user)
-     console.log(result);
+     const userCollection = client.db('tourservice').collection('users');
+      app.get('/services/upcoming', async(req, res)=>{
+       const query = {}
+       const cursor = userCollection.find(query);
+       const services= await cursor.limit(3).toArray();
+       res.send(services);
+     })
+     
+      app.get('/services', async(req, res)=>{
+       const query = {}
+       const cursor = userCollection.find(query);
+       const services= await cursor.toArray();
+       res.send(services);
+     })
+
+     app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const service = await userCollection.findOne(query);
+      res.send(service);
+   })
+
+     
    }
    finally{
 
